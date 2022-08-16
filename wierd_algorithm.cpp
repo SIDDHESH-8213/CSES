37d8f5c6993a321c11290879786f02848f70e529
SIDDHESH-8213 <75976447+SIDDHESH-8213@users.noreply.github.com>
2022-08-16 19:09:38 +0530

    Introductory problems

---
+++ wierd_algorithm.cpp
@@ -0,0 +1,22 @@
+#include <bits/stdc++.h>
+using namespace std;
+ 
+typedef long long ll;
+ 
+ 
+void solve(){
+	ll n;
+	cin>>n;
+	cout<<n<<" ";
+	while(n != 1){
+		if(n%2 == 0) n /= 2;
+		else n = 3*n + 1;
+		cout<<n<<" ";
+	}
+	cout<<"\n";
+}
+ 
+int main(){
+	solve();
+	return 0;
+}
