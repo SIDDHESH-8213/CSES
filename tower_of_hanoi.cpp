0212251ac99c8fce987a04893122cb211888dfb2
SIDDHESH-8213 <75976447+SIDDHESH-8213@users.noreply.github.com>
2022-08-16 15:10:08 +0530

    Add files via upload

---
+++ tower_of_hanoi.cpp
@@ -0,0 +1,35 @@
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+const ll mod = 1e9+7;
+
+void th(ll n, ll s, ll e, ll h, vector<vector<ll>> &vt){
+    if(n == 1) {
+        //cout<<s<<" "<<e<<"\n";
+        vt.push_back({s, e});
+        return;
+    }
+
+    th(n-1, s, h, e, vt);
+    vt.push_back({s, e});
+    th(n-1, h, e, s, vt);
+}
+
+void solve(){
+    ll n;
+    cin>>n;
+    vector<vector<ll>> vt;
+    th(n, 1, 3, 2, vt);
+    cout<<vt.size()<<"\n";
+    for(auto i : vt){
+        for(auto it : i){
+            cout<<it<<" ";
+        }
+        cout<<"\n";
+    }
+}
+
+int main(){
+    solve();
+    return 0;
+}
